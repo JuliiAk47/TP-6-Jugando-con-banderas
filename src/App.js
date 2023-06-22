14ca4ba584810bd69e0e03ba0d8b98ded72e5c16
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-22 10:28:35 -0300

    terminado sin extras y feo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState([]);
   const [puntaje, setPuntaje] = useState(0);
   const [paises, setPaises] = useState([]);
   const [paisRandom, setPaisRandom] = useState({});
@@ -16,21 +17,36 @@ function App() {
 
         const randomElement = paisesResponse[Math.floor(Math.random() * paisesResponse.length)];
         setPaisRandom(randomElement);
+        console.log(response.data.data);
+        console.log(randomElement);
+      
       });
   }, []);
 
   const verificarRespuesta = ()=>{
+    if (name===paisRandom.name) {
+      console.log("respuesta correcta");
+      setPuntaje(puntaje+10);
+      const randomElement = paises[Math.floor(Math.random() * paises.length)];
+      setPaisRandom(randomElement);
+
+    }
+    else{
+      console.log("respuesta incorrecta");
+      setPuntaje(puntaje-1);
+    }
     
   }
 
 
   return (
     <div>
-      <h1>{ paisRandom.name }</h1>
+      <h1>Adivina el pais</h1>
       <img  src={paisRandom.flag} className='img' />
-      <input type="text" id="pais" placeholder='Adivina la bandera'></input>
-      <button type='submit' onClick={verificarRespuesta}></button>
-      
+      <h2>Puntaje: {puntaje}</h2>
+      <input type="text" id="pais" placeholder='Adivina la bandera' onKeyUp={(e) => setName(e.target.value)}></input>
+      <button type='submit' onClick={verificarRespuesta}>Enviar</button>
+    
     </div>
   );
 }
